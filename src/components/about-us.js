a075811aa3280bb0323948986a0189b232921cd1
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-03-31 16:43:06 +1100

    about-us

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const About =({style})=>{
+const About =({styleText,styleWrap})=>{
 
 
 
@@ -10,20 +10,20 @@ const About =({style})=>{
    
     return (
 
-        
-    <div  className="about-para" style={style} >
-            
-            <h2 className="h3  text-center ">Hi, my name's Claudia.</h2>
-            <h4 className="h4  text-center" >I'm a specialised Speech Pathologist and the founder of Pronounce It.</h4>
-            <p><br/>I've been working in the area of English Pronunciation and Accent for over thirteen years. Many people know that a Speech Pathologist is someone who works with children with speech difficulties or someone who works in hospitals with people who are ill. Yes, they certainly do this. However, after graduating with a Bachelor of Speech Pathology in 2003 and initially working in both of the above areas, I decided that working with accent and pronunciation was where my passion lied and studied further to specialise in this area. </p>
-            <p> I have a great love of traveling and have travelled all over the world. Some of the places include: Peru, Brazil, Bolivia, Chile, Turkey, Spain, Holland, Italy, France, England, Morocco and more! I have a passion for travel because I love meeting people from other cultures and traditions and seeing and hearing about their history and stories. I therefore count myself lucky to work in the area I do. Now, instead of travelling myself, these wonderful people and their cultures come to me!</p>
-            <p>I usually run my sessions on Collins St in Melbourne CBD or St Kilda, but am currently running them online due to COVID19. My approach is relaxed with a great sense of fun and aim to make my clients feel as relaxed and comfortable as possible! I like to work hard with my clients but I also believe it's important for speech training to be enjoyable and for my clients to look forward to each session. </p>
-           <br/>
-            <p className="font-italic text-center" >I'm always very happy to chat so if you have any questions about me or the service, please don't hesitate to contact me.</p>
+<div className="about-para-wrapper" style={styleWrap} >
+        <div  className="about-para" style={styleText}>
+                
+                <h2 className="h3  text-center ">Hi, my name's Claudia.</h2>
+                <h4 className="h4  text-center" >I'm a specialised Speech Pathologist and the founder of Pronounce It.</h4>
+                <p><br/>I've been working in the area of English Pronunciation and Accent for over thirteen years. Many people know that a Speech Pathologist is someone who works with children with speech difficulties or someone who works in hospitals with people who are ill. Yes, they certainly do this. However, after graduating with a Bachelor of Speech Pathology in 2003 and initially working in both of the above areas, I decided that working with accent and pronunciation was where my passion lied and studied further to specialise in this area. </p>
+                <p> I have a great love of traveling and have travelled all over the world. Some of the places include: Peru, Brazil, Bolivia, Chile, Turkey, Spain, Holland, Italy, France, England, Morocco and more! I have a passion for travel because I love meeting people from other cultures and traditions and seeing and hearing about their history and stories. I therefore count myself lucky to work in the area I do. Now, instead of travelling myself, these wonderful people and their cultures come to me!</p>
+                <p>I usually run my sessions on Collins St in Melbourne CBD or St Kilda, but am currently running them online due to COVID19. My approach is relaxed with a great sense of fun and aim to make my clients feel as relaxed and comfortable as possible! I like to work hard with my clients but I also believe it's important for speech training to be enjoyable and for my clients to look forward to each session. </p>
+                <br/>
+                <p className="font-italic text-center" >I'm always very happy to chat so if you have any questions about me or the service, please don't hesitate to contact me.</p>
 
-       
-    </div>
-       
+        
+        </div>
+    </div> 
   )
  }
 
--- src/components/title.js
+++ src/components/title.js
@@ -6,40 +6,54 @@ import About from '../components/about-us';
 class Title extends React.Component{
   constructor(props){
     super(props)
-   this.state={ buttonClick: false,  dot:"",opacity: 0}
+   this.state={ buttonClick: false,  dot:"",opacity: 0,width: '0vw',height: '0%',boxShadow: '0'}
    this.handleAboutus=this.handleAboutus.bind(this);
   }
 
 
   handleAboutus(){
-      this.setState({buttonClick: true,dot: '<---- ',opacity: 1});
+      this.setState({
+        buttonClick: true,
+        dot: '<---- ',
+        opacity: 1,
+        width: '70vw',
+        height: '100%', 
+        boxShadow: '2px 2px 5px 1px rgb(21, 110, 184)'
+      })
      
     }
 
   
   render(){
 
-    const{buttonClick,dot,opacity}=this.state
-   
+    const{buttonClick,dot,opacity,width,height,boxShadow}=this.state
+    console.log(width);
+    console.log(height);
     return(
 
 <div className="main">
         <h1 className="title">pr<span className="letter">o</span>n<span className="letter">o</span>unce it! </h1>
         <nav className="navbar ">
-              <button  type="button" className="button nav-bg ">Home</button>
+              <button type="button" className="button nav-bg ">Home</button>
               <button onClick={this.handleAboutus}  type="button" className="button nav-bg ">{dot}About us</button>
-
+              
               { buttonClick ? null :<button type="button" className="button nav-bg ">Services</button>}
               { buttonClick ? null: <button type="button" className="button nav-bg ">Testimonials</button>}
                { buttonClick ? null:<button type="button" className="button nav-bg ">Contact us</button>}
                { buttonClick ? null: <button type="button" className="button nav-bg ">FAQs</button>}
-               <About  style={{opacity:opacity,transition: 'opacity 0.3s linear 0.1s'}}/>
+        </nav>
+
+        <About  styleText={{opacity:opacity, transition : 'opacity 0.5s ease 0.39s'}}
+             styleWrap={{width: width, height: height, transitionProperty: 'width , height' , transitionDuration: '0.4s,0.4s', boxShadow: boxShadow }}
+        />   
+       
+</div>    
+        
+               
               
              
-        </nav>
          
          
-</div>       
                   
   )
 }
