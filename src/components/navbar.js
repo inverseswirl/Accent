c952df645b42f8bc9e12c3afeee8b758ef1a723e
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-04-09 20:09:34 +1000

    latest

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {BiComment} from 'react-icons/bi';
 import {RiHome2Line} from 'react-icons/ri';
 import { CgList} from 'react-icons/cg';
+import {HiOutlineUsers} from 'react-icons/hi'
+import {IoCallOutline} from 'react-icons/io5'
+
 
 
 class Navbar extends React.Component{
@@ -14,15 +17,17 @@ return(
     
         <nav className="navbar ">
         
-        
               <button type="button" className="button nav-bg ">
-                 <RiHome2Line className="bg-transparent" color='#ffffff' size={35} />  Home </button>
+                 <RiHome2Line className="bg-transparent" color='#ffffff' size={29} />  Home </button>
               <button  type="button" className="button nav-bg ">
-                  <BiComment className="bg-transparent"color='#ffffff' size={35}/> About us </button>
+                  <BiComment className="bg-transparent"color='#ffffff' size={29}/> About us </button>
               <button type="button"  className="button nav-bg ">
-                  <CgList className="bg-transparent"color='#ffffff' size={35}/> Services </button>
-              <button type="button"  className="button nav-bg ">Testimonials</button>
-            
+                  <CgList className="bg-transparent"color='#ffffff' size={28}/> Services </button>
+              <button type="button" className="button nav-bg ">
+                 <IoCallOutline className="bg-transparent"color='#ffffff' size={30}/> Contact us</button>
+              <button type="button"  className="button nav-bg ">
+              <HiOutlineUsers className="bg-transparent"color='#ffffff' size={29}/>Testimonials</button>
+             
               {/* { aboutClick ? <button type="button"  style={{display: display}}className="button nav-bg ">Services</button>: null}
               { aboutClick ? null: <button type="button" style={{display: display}}className="button nav-bg ">Testimonials</button>}
                { aboutClick ? null:<button type="button" style={{display: display}} className="button nav-bg ">Contact us</button>}
