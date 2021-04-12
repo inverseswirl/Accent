f005dd560e8d37d7d091e4d39b82e8ed7e4d5a02
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-04-12 17:50:55 +1000

    update

--- src/components/about-page.js
+++ src/components/about-page.js
@@ -26,18 +26,17 @@ return (
         {click? <Back />:null}
     <div className="about-para-wrapper" id="about">
           <div  className="about-para" >
-                  <p className="text-center ">Hi, we assist people with better communication </p>
-                  <p><br/>We have been working in the area of Communication and Coaching for about 20 years.  We aim to deliver best results for our clients.   </p>
+                  <p className="text-center">Hi, we assist people with better communication </p>
+                  <p>We have been working in the area of Communication and Coaching for about 20 years.  We aim to deliver best results for our clients.</p>
                   <br/>
                   <h3> Why choose us?</h3>
                    <ul>
                       <li>Intensive, tailor-made sessions delivered on your terms and on your schedule</li>
                       <li>Highly experienced, calm and empathic coaches</li>
                       <li>Honest, direct and focused feedback</li>
-                      
-                  </ul><br/>
+                  </ul>
+                  <br/>
                  <h3>Meet our Coaches</h3>
-                 
                  
                  <div className="about-coaches">
                     <FcBusinesswoman clasName="" size={150}/>
@@ -45,15 +44,14 @@ return (
                     <FcBusinessman className="" size={150}/>
                     <br/>
                     <FcBusinessman className="" size={150}/>
-                <FcBusinesswoman   className="" size={150}/>
+                   <FcBusinesswoman   className="" size={150}/>
                 </div>
-                
-                 <br/>
+                <br/>
                   <p className="font-italic text-center" >We are happy to connect with you over telephone or chat in person, so if you have any questions about our services, please contact us.</p>
-          </div>
-          <div>
-
-          </div>
+                 
+            </div>
+                
+                      
     </div> 
     <span className="line"></span>
           
--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,9 @@ const Home =(props)=>{
      
         <p className="home">It is proven that success both personally and professionally is about effective communication skills. Such skills are highly prized in creative, knowledge-based workforces. As technology shifts the working landscape, and teams become increasingly diverse, good communication in all its forms becomes more than a nice to have. Bloomberg’s Annual Skills Report regularly places Communication as the number one skill actively considered by recruiting firms.
         <br/>
+        <br/>
         At Accent we help our clients build techniques and confidence for handling a range of communication styles, people and situations.</p>
+    
            
            
             
